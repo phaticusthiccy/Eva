39c89c28b65c66649a8e64b050c8613c1503178b
Thiccy <birhic023@gmail.com>
2021-07-17 13:50:34 +0300

    Create information.py

---
+++ library/information.py
@@ -0,0 +1,20 @@
+# The entire Eva Application is Copyright ©2021 by Phaticusthiccy.
+# The Eva site may not be copied or duplicated in whole or part by any means without express prior agreement in writing or unless specifically noted on the site.
+# Some photographs or documents contained on the application may be the copyrighted property of others; acknowledgement of those copyrights is hereby given.
+# All such material is used with the permission of the owner.
+# All Copyright Belong to Phaticusthiccy - (2017-2021) Eva 
+# All Rights Reserved.
+
+info_one = 'Author: Phaticusthiccy'
+info_two = 'Version: Global'
+info_three = 'Info: Eva Neural Assistant - Chatbot // Machine Learning - Deep Learning'
+info_npm = 'NPM: true'
+info_cli = 'Cli: false'
+info_langs = 'JavaScript, Node, Typescript'
+
+print(info_one, end='\n')
+print(info_two, end='\n')
+print(info_three, end='\n')
+print(info_npm, end='\n')
+print(info_cli, end='\n')
+print(info_langs, end='')
