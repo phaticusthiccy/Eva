4d410790837eb1c575508c70c5eb67ae8a420b9b
Thiccy <birhic023@gmail.com>
2021-07-17 13:53:37 +0300

    Create information_v.js

---
+++ library/information_v.js
@@ -0,0 +1,17 @@
+/*
+The entire Eva Application is Copyright ©2021 by Phaticusthiccy.
+The Eva site may not be copied or duplicated in whole or part by any means without express prior agreement in writing or unless specifically noted on the site.
+Some photographs or documents contained on the application may be the copyrighted property of others; acknowledgement of those copyrights is hereby given.
+All such material is used with the permission of the owner.
+All Copyright Belong to Phaticusthiccy - (2017-2021) Eva 
+All Rights Reserved.
+*/
+
+var data = {
+  author: 'Phaticusthiccy',
+  version: 'Global',
+  npm: true,
+  cli: false,
+  langs: ['JavaScript', 'Node', 'Typescript']
+};
+console.log(data);
