54c3d8fb233ac4155b8ffe7fcde10946ef4f9672
Thiccy <birhic023@gmail.com>
2021-07-21 15:25:25 +0300

    Create map[exp].el

---
+++ eva/map[exp].el
@@ -0,0 +1,7 @@
+;; Object SRecode Map
+;; EIEIO PERSISTENT OBJECT
+(srecode-map "SRecode Map"
+  :file "srecode-map.el"
+  :files '((`${HOME}` . wisent-grammar-mode) (`${HOME}` . texinfo-mode) (`${HOME}` . srecode-template-mode) (`${HOME}` . graphviz-dot-mode) (`${HOME}`  . makefile-mode) (`${HOME}`  . java-mode) (`${HOME}` . html-mode) (`${HOME}`  . emacs-lisp-mode) (`${HOME}`  . default) (`${HOME}`  . c++-mode) (`${HOME}` . default) (`${HOME}` . c-mode) (`${HOME}` . arduino-mode) (`${HOME}`  . java-mode))
+  :apps '((tests (`${HOME}` . srecode-template-mode))
+)
