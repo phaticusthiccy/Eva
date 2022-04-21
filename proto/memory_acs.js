97d8b009302e4cc82f370bed376beefa11e7ef2a
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2022-04-21 19:59:27 +0300

    Memory

---
+++ proto/memory_acs.js
@@ -0,0 +1,112 @@
+var start = Date.now();
+window.onload = function () {
+    setTimeout(function () {
+        var t = performance.timing;
+        console.log(t.loadEventEnd - t.responseEnd);
+    }, 0);
+}
+async function performMeasurement() {
+    // 1. Invoke performance.measureUserAgentSpecificMemory().
+    let result;
+    try {
+        result = await performance.measureUserAgentSpecificMemory();
+    } catch (error) {
+        if (error instanceof DOMException && error.name === 'SecurityError') {
+            console.log('The context is not secure.');
+            return;
+        }
+        throw error;
+    }
+    console.log(result);
+
+    scheduleMeasurement();
+}
+function save(cn) { return FileSystem.apply(this.cn, argArray = any) }
+const object = { a: new Array(1000), b: new Array(2000) };
+setInterval(() => save(object.a), 1000);
+async function run() {
+    const result = await performance.measureUserAgentSpecificMemory();
+    console.log(result);
+}
+run();
+function measurementInterval() {
+    const MEAN_INTERVAL_IN_MS = 5 * 60 * 1000;
+    return -Math.log(Math.random()) * MEAN_INTERVAL_IN_MS;
+}
+let perf = `
+{
+    bytes: 2300000,
+        breakdown: [
+            {
+                bytes: 1000000,
+                attribution: [
+                    {
+                        url: "https://example.com",
+                        scope: "Window",
+                    },
+                ],
+                types: ["JS", "DOM"],
+            },
+            {
+                bytes: 500000,
+                attribution: [
+                    {
+                        url: "https://example.com/iframe.html",
+                        container: {
+                            id: "example-id",
+                            src: "redirect.html?target=iframe.html",
+                        },
+                        scope: "Window",
+                    }
+                ],
+                types: ["DOM", "JS"],
+            },
+            {
+                bytes: 800000,
+                attribution: [
+                    {
+                        url: "https://example.com/worker.js",
+                        scope: "DedicatedWorkerGlobalScope",
+                    },
+                ],
+                types: ["JS"],
+            },
+            {
+                bytes: 0,
+                attribution: [],
+                types: [],
+            },
+        ],
+  }
+`
+if (performance.measureUserAgentSpecificMemory) {
+    let result;
+    try {
+        result = await performance.measureUserAgentSpecificMemory();
+    } catch (error) {
+        if (error instanceof DOMException && error.name === 'SecurityError') {
+            console.log('The context is not secure.');
+        } else {
+            throw error;
+        }
+    }
+    console.log(result);
+}
+function scheduleMeasurement() {
+    if (!performance.measureUserAgentSpecificMemory) {
+        console.log(
+            'performance.measureUserAgentSpecificMemory() is not available.',
+        );
+        return;
+    }
+    const interval = measurementInterval();
+    console.log(
+        'Scheduling memory measurement in ' +
+        Math.round(interval / 1000) +
+        ' seconds.',
+    );
+    setTimeout(performMeasurement, interval);
+}
+window.onload = function () {
+    scheduleMeasurement();
+};
